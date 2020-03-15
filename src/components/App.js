a7116c695131f47fc277ea0b4ac53ea2757e473f
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2020-03-15 20:48:10 +0300

    add form and banner

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,52 @@
-import React, { PureComponent } from 'react'
-import Nav from './Nav'
+import React, { Component } from 'react'
 import '../css/main.css'
 
-class App extends PureComponent {
+import Nav from './Nav'
+import Form from './Form'
+import Banner from './Banner'
+
+import BannerModel from '../models/BannerModel'
+
+class App extends Component {
+  state = {
+    currentBanner: new BannerModel(),
+    savedBanner: null
+  }
+
+  updateBanner = updatedBanner => {
+    this.setState({ currentBanner: updatedBanner })
+  }
+
+  submitBanner = event => {
+    event.preventDefault()
+    this.setState({ savedBanner: this.state.currentBanner })
+    this.props.history.push('/preview')
+  }
+
+  isPreviewAvailable() {
+    return this.state.savedBanner instanceof BannerModel
+  }
+
   render() {
     return (
       <section className="layout">
         <aside className="layout__sidebar">
-          <Nav />
+          <Nav isPreviewAvailable={this.isPreviewAvailable()} />
         </aside>
         <main className="layout__content">
-          <section id="params" className="screen"></section>
+          <section id="params" className="screen">
+            <Form
+              banner={this.state.currentBanner}
+              updateBanner={this.updateBanner}
+              submitBanner={this.submitBanner}
+            />
+          </section>
 
-          <section id="preview" className="screen"></section>
+          <section id="preview" className="screen">
+            {this.isPreviewAvailable() && (
+              <Banner banner={this.state.savedBanner} />
+            )}
+          </section>
         </main>
       </section>
     )
