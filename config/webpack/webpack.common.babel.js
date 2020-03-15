16bb5d414d816006681c3f91067b6aa2a42dcbd3
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2020-03-16 00:17:53 +0300

    add autoprefixer, fixed indentations

--- config/webpack/webpack.common.babel.js
+++ config/webpack/webpack.common.babel.js
@@ -1,30 +1,31 @@
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+const autoprefixer = require('autoprefixer')
 
-import paths from './paths';
-import rules from './rules';
+import paths from './paths'
+import rules from './rules'
 
 module.exports = {
-    entry: paths.entryPath,
-    module: {
-        rules
-    },
-    resolve: {
-        modules: ['src', 'node_modules'],
-        extensions: ['*', '.js', '.scss', '.css']
-    },
-    plugins: [
-        new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({
-            template: paths.templatePath,
-            minify: {
-                collapseInlineTagWhitespace: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: true,
-                minifyURLs: true,
-                removeComments: true,
-                removeAttributeQuotes: true
-            }
-        })
-    ]
-};
+  entry: paths.entryPath,
+  module: {
+    rules
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.scss', '.css']
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: paths.templatePath,
+      minify: {
+        collapseInlineTagWhitespace: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: true,
+        minifyURLs: true,
+        removeComments: true,
+        removeAttributeQuotes: true
+      }
+    })
+  ]
+}
--- config/webpack/webpack.prod.babel.js
+++ config/webpack/webpack.prod.babel.js
@@ -1,18 +1,18 @@
-import CleanWebpackPlugin from 'clean-webpack-plugin';
+import CleanWebpackPlugin from 'clean-webpack-plugin'
 
-import paths from './paths';
+import paths from './paths'
 
 module.exports = {
-    mode: 'production',
-    output: {
-        filename: `${paths.jsFolder}/[name].[hash].js`,
-        path: paths.outputPath,
-        chunkFilename: '[name].[chunkhash].js'
-    },
-    plugins: [
-        new CleanWebpackPlugin([paths.outputPath.split('/').pop()], {
-            root: paths.root
-        })
-    ],
-    devtool: 'source-map'
-};
+  mode: 'production',
+  output: {
+    filename: `${paths.jsFolder}/[name].[hash].js`,
+    path: paths.outputPath,
+    chunkFilename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin([paths.outputPath.split('/').pop()], {
+      root: paths.root
+    })
+  ],
+  devtool: 'source-map'
+}
